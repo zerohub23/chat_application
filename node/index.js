70f607770236b6e3a055adf85f4d9ef77ad1701c
raksha <raksha@gmail.com>
2021-03-10 00:41:32 +0530

    first commit

---
+++ node/index.js
@@ -0,0 +1,13 @@
+const io=require('socket.io')(8000);
+
+const user={};
+io.on('connection',socket=>{
+    socket.on('new-user-joined',name=>{
+       user[socket.id]= name;
+       socket.broadcast.emit('user-joined the chat',name);
+    });
+
+    socket.on('send',message=>{
+        socket.broadcast.emit('recieve',{messag:message,name:user[socket.id]})
+    });
+})
